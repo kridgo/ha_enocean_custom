e8ecac38f12991e3ead4364faa35ddace9595a6e
awdw <awdwa>
2023-03-17 05:00:59 +0100

    Add double rocker send mode to switch.py

--- custom_components/enocean_custom/switch.py
+++ custom_components/enocean_custom/switch.py
@@ -18,12 +18,14 @@ from .device import EnOceanEntity
 
 CONF_CHANNEL = "channel"
 DEFAULT_NAME = "EnOcean Switch"
+CONF_SWITCH_TYPE = "switch_type"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_ID): vol.All(cv.ensure_list, [vol.Coerce(int)]),
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
         vol.Optional(CONF_CHANNEL, default=0): cv.positive_int,
+        vol.Optional(CONF_SWITCH_TYPE, default="default"): cv.string,
     }
 )
 
@@ -68,21 +70,23 @@ async def async_setup_platform(
     channel = config.get(CONF_CHANNEL)
     dev_id = config.get(CONF_ID)
     dev_name = config.get(CONF_NAME)
+    switch_type = config.get(CONF_SWITCH_TYPE)
 
     _migrate_to_new_unique_id(hass, dev_id, channel)
-    async_add_entities([EnOceanSwitch(dev_id, dev_name, channel)])
+    async_add_entities([EnOceanSwitch(dev_id, dev_name, channel, switch_type)])
 
 
 class EnOceanSwitch(EnOceanEntity, SwitchEntity):
     """Representation of an EnOcean switch device."""
 
-    def __init__(self, dev_id, dev_name, channel):
+    def __init__(self, dev_id, dev_name, channel, switch_type):
         """Initialize the EnOcean switch device."""
         super().__init__(dev_id, dev_name)
         self._light = None
         self._on_state = False
         self._on_state2 = False
         self.channel = channel
+        self.switch_type = switch_type
         self._attr_unique_id = generate_unique_id(dev_id, channel)
 
     @property
@@ -97,27 +101,61 @@ class EnOceanSwitch(EnOceanEntity, SwitchEntity):
 
     def turn_on(self, **kwargs: Any) -> None:
         """Turn on the switch."""
-        optional = [0x03]
-        optional.extend(self.dev_id)
-        optional.extend([0xFF, 0x00])
-        self.send_command(
-            data=[0xD2, 0x01, self.channel & 0xFF, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00],
-            optional=optional,
-            packet_type=0x01,
-        )
-        self._on_state = True
+        if self.switch_type == "RPS":
+            if self.channel == 1:
+                val_pushed = 0x10
+            else:
+                val_pushed = 0x50
+            #self.send_command([0xF6, val_pushed, self.dev_id, 0x30], [], 0x01)
+            #self.send_command([0xF6, 0x00, self.dev_id, 0x20], [], 0x01)
+            command = [0xF6, val_pushed]
+            command.extend(self.dev_id)
+            command.extend([0x30])
+            self.send_command(command, [], 0x01)
+            command = [0xF6, 0x00]
+            command.extend(self.dev_id)
+            command.extend([0x20])
+            self.send_command(command, [], 0x01)
+            self._on_state = True
+        else:
+            optional = [0x03]
+            optional.extend(self.dev_id)
+            optional.extend([0xFF, 0x00])
+            self.send_command(
+                data=[0xD2, 0x01, self.channel & 0xFF, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00],
+                optional=optional,
+                packet_type=0x01,
+            )
+            self._on_state = True
 
     def turn_off(self, **kwargs: Any) -> None:
         """Turn off the switch."""
-        optional = [0x03]
-        optional.extend(self.dev_id)
-        optional.extend([0xFF, 0x00])
-        self.send_command(
-            data=[0xD2, 0x01, self.channel & 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
-            optional=optional,
-            packet_type=0x01,
-        )
-        self._on_state = False
+        if self.switch_type == "RPS":
+            if self.channel == 1:
+                val_pushed = 0x30
+            else:
+                val_pushed = 0x70
+            #self.send_command([0xF6, val_pushed, self.dev_id, 0x30], [], 0x01)
+            #self.send_command([0xF6, 0x00, self.dev_id, 0x20], [], 0x01)
+            command = [0xF6, val_pushed]
+            command.extend(self.dev_id)
+            command.extend([0x30])
+            self.send_command(command, [], 0x01)
+            command = [0xF6, 0x00]
+            command.extend(self.dev_id)
+            command.extend([0x20])
+            self.send_command(command, [], 0x01)
+            self._on_state = False
+        else:
+            optional = [0x03]
+            optional.extend(self.dev_id)
+            optional.extend([0xFF, 0x00])
+            self.send_command(
+                data=[0xD2, 0x01, self.channel & 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                optional=optional,
+                packet_type=0x01,
+            )
+            self._on_state = False
 
     def value_changed(self, packet):
         """Update the internal state of the switch."""
